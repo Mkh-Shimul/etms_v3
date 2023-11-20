6532e730606cf62dd7b8146e5e51c790d3f33f86
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-21 01:26:23 +0600

    formModule added

--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UserCreateComponent } from './user-create/user-create.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 
 @NgModule({
   declarations: [
     UserCreateComponent,
-    UserListComponent
+    UserListComponent,
+    UserEditComponent
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    FormsModule
   ]
 })
 export class UsersModule { }
