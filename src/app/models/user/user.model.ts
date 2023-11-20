a09e38ed6de97948e575fee11327279709d30b08
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-21 01:31:41 +0600

    model added for user component(s)

---
+++ src/app/models/user/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    id!: number;
+    fullName!: string;
+    email!: string;
+    userName!: string;
+    password!: string;
+    roleName!: number;
+    userRoleId!: number;
+    isActive!: boolean;
+}
