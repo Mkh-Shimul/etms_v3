0a619db3beff0ee3791e2f4a9bb57f340ebab89c
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-21 01:29:03 +0600

    user creation component done

--- src/app/components/users/user-create/user-create.component.ts
+++ src/app/components/users/user-create/user-create.component.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
+import { userDTO } from 'src/app/models/authentication/user-dto.model';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-user-create',
@@ -8,15 +12,39 @@ import { Component } from '@angular/core';
 })
 export class UserCreateComponent {
 
-  userRoles: any = []
+  userRoles: any = 0;
+  usrRqst: userDTO;
 
-  constructor(private _http: HttpClient){}
+  constructor(private _userService: UserService, private _toast: NgToastService
+    , private _route: Router){
+    this.usrRqst = new userDTO();
+  }
 
   ngOnInit() {
     this.getUserRoles();
   }
 
   getUserRoles() {
-    alert("Get user Roles from api")
+    this._userService.GetAllUserRoles().subscribe({
+      next: (response) => {
+        this.userRoles = response;
+        console.log(response);
+      }, error: (err) => {
+        console.log(err)
+      }, complete: () => {
+        console.log("completed");
+      }
+    })
+  }
+
+  createUser() {
+    this._userService.CreateUser(this.usrRqst).subscribe({
+      next: (response) => {
+        this._toast.success({ detail: "Success", summary: "User Created Successully"});
+        this._route.navigate(['/users']);
+      }, error: (err) => {
+        this._toast.error({ detail: "Failed", summary: "User Creation Failed"});
+      }
+    })
   }
 }
