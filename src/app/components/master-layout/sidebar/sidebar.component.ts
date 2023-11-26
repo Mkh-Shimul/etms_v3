2ad70c7878b41fa18e2aa9038153deb9ec755d34
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-27 02:08:11 +0600

    menu list are loaded with based on userRole from token value

--- src/app/components/master-layout/sidebar/sidebar.component.ts
+++ src/app/components/master-layout/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
+import { NotExpr } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 import { JsonWebTokenService } from 'src/app/services/json-web-token.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,11 +11,29 @@ import { JsonWebTokenService } from 'src/app/services/json-web-token.service';
 })
 export class SidebarComponent {
   userName: any;
-  constructor(private jwtService: JsonWebTokenService) {}
+  menuList: any = [];
+  currentRoleId: any;
+  
+  constructor(private _jwtService: JsonWebTokenService, private _authService: AuthenticationService) { }
+  
   ngOnInit() {
     let token = localStorage.getItem('token');
-    let decodejwt = this.jwtService.DecodeToken(token);
-    this.userName = this.jwtService.GetUser();
+    let decodejwt = this._jwtService.DecodeToken(token);
+    this.userName = this._jwtService.GetUser();
     console.log("HEADER: ", this.userName);
+    this.getMenubyRoleId();
+    console.log("ROLE: ", parseInt(this.currentRoleId))
+  }
+  
+  getMenubyRoleId() {
+    this.currentRoleId = this._jwtService.GetUserRoleId();
+    this._authService.GetMenubyRoleId(parseInt(this.currentRoleId)).subscribe({
+      next: (response) => {
+        this.menuList= response;
+        console.log(response)
+      }, error: (err) => {
+        console.log(err);
+      }
+    })
   }
 }
