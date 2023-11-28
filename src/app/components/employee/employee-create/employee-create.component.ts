0b9c53ccdd547d58ab2dce47a52b3f1ec155d9a4
Mkh-Shimul <mkhshimul7@gmail.com>
2023-11-29 01:28:40 +0600

    bulk upload data saving partially done

--- src/app/components/employee/employee-create/employee-create.component.ts
+++ src/app/components/employee/employee-create/employee-create.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 import { Employee } from 'src/app/models/employee/employee.model';
 import { EmployeeService } from 'src/app/services/employee/employee.service';
 
@@ -10,18 +11,48 @@ import { EmployeeService } from 'src/app/services/employee/employee.service';
 })
 export class EmployeeCreateComponent {
   employeeRqst: Employee;
+  isFileUploadEnable: boolean = false;
+  excelData: File | undefined;
 
-  constructor(private _employeeService: EmployeeService, private _route: Router){ this.employeeRqst = new Employee() }
-  
-  createEmployee(){
-    this._employeeService.CreateEmployee(this.employeeRqst).subscribe({
-      next: (response) => {
-        alert("Data Saved Successfully");
-        // alert(response);
-        this._route.navigate(['/employee'])
-      }, error: (err) => {
-        console.log(err);
+  constructor(private _employeeService: EmployeeService, private _route: Router,
+    private _toast: NgToastService) { this.employeeRqst = new Employee() }
+
+  createEmployee() {
+    if (this.isFileUploadEnable === true) {
+      if (this.excelData === undefined) {
+        this._toast.error({ detail: "Error", summary: "No File has been selected" })
+      } else {
+        // file upload functionalities
+        console.log(this.excelData);
       }
-    })
+    } else {
+      this._employeeService.CreateEmployee(this.employeeRqst).subscribe({
+        next: (response) => {
+          alert("Data Saved Successfully");
+          // alert(response);
+          this._route.navigate(['/employee'])
+        }, error: (err) => {
+          console.log(err);
+        }
+      })
+    }
+
+
   }
+
+  excelFileUploadEnable(event: any) {
+    event.target.checked ? this.isFileUploadEnable = true : this.isFileUploadEnable = false;
+  }
+
+  //#region Helper Function
+  onFileChange(event: any) {
+    const fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      this.excelData = fileList[0];
+    } else {
+      // No file selected, reset the variable
+      this.excelData = undefined;
+    }
+  }
+  //#endregion
 }
